b76dc79736e6ce0cd71b7f3a2d0717aaa5285cd2
YeoheungMinSsi <20161538@office.dongseo.ac.kr>
2024-06-05 20:16:17 +0900

    1. 파일별로 정리함  1-1 HTML, CSS, JS 파일별로 정리 2. 필요없는 파일 정리

--- sss.js
+++
@@ -1,8 +0,0 @@
-function toggleList(num) {
-    var list = document.getElementById("myList" + num);
-    if (list.style.display === "none") {
-        list.style.display = "block";
-    } else {
-        list.style.display = "none";
-    }
-}
